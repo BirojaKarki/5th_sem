77c4ce12f9315faa888a043c619b29061e700ec2
BirojaKarki <karkibiroja@gamil.com>
2024-06-06 08:21:51 +0545

    first commit

---
+++ stringmethod.js
@@ -0,0 +1,21 @@
+var text="I am curently involved in 5th sem of BSC.Csit"
+console.log(text);
+//print the length of the string
+console.log("The length of text is ",text.length);
+//print the character at index 5 of the given string
+console.log("The charcter at index 5 is :",text.charAt(5));
+//printthe charcater code  at index 5 of the given string
+console.log("The charcter at index 5 is :",text.charCodeAt(5));
+//print the string subsrtirg
+console.log("The substring of text is :",text.substring(1,4));
+//print into Uppercase
+console.log("The string in uppercase is :",text.toUpperCase());
+//print in lowercase
+console.log("The  string in lowercase :",text.toLowerCase());
+var text2="Hello"
+console.log(text2);
+console.log("The contcation of string is :",text2.concat(" ",text));
+//repeat the text
+console.log("The repeated text is :",text.repeat(2));
+//replace the text
+console.log("Replace :",text.replace("BSC.Csit","BIM"));
