f4c92cd3d5e8e46461e57622456cfd7aa61d45bf
BirojaKarki <karkibiroja@gamil.com>
2024-06-18 08:05:37 +0545

    first commit

---
+++ ifelse.js
@@ -0,0 +1,43 @@
+  var num=20;
+
+if(num>30){
+    console.log('The number is greater then 30');
+
+}
+else if(num<30){
+    console.log('The number is smaller then 30');
+
+}
+else{
+    console.log("equal number");
+}
+ 
+if(num%2!==0){
+    console.log("The number is odd");
+}
+else if(num%2===0){
+    console.log('The number is even');
+}
+
+math=39;
+science=68;
+biology=56;
+nepali=78;
+english=78;
+marks=((math+science+biology+nepali+english)/500)*100;
+if(marks<50 && marks>60){
+    console.log("Second division");
+}
+else if(marks>60&&marks<80){
+    console.log("First Division");
+}
+else if(marks>80){
+    console.log('Distinction');
+}
+else{
+    console.log('FAil');
+}
+
+
+
+
